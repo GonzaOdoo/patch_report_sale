5317972afd609758e3711ee4e48e97adac937a1e
GonzaOdoo <zelayagonzalo33@gmail.com>
2025-08-06 19:59:46 -0300

    Inicializado modulo

---
+++ models/report_sale.py
@@ -0,0 +1,20 @@
+# en tu módulo personalizado, por ejemplo: sale_report_custom/models/sale_report.py
+
+from odoo import api, fields, models
+
+class SaleReport(models.Model):
+    _inherit = 'sale.report'
+
+    cliente_final = fields.Char(string='Cliente Final', readonly=True)
+
+    def _select_sale(self):
+        select = super()._select_sale()
+        # Añadimos el campo personalizado
+        select += ", l.x_studio_cliente_final AS cliente_final"
+        return select
+
+    def _group_by_sale(self):
+        group_by = super()._group_by_sale()
+        # Aseguramos que el campo esté en GROUP BY
+        group_by += ", l.x_studio_cliente_final"
+        return group_by
